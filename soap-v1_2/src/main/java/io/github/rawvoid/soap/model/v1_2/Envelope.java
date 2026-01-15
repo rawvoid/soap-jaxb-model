87cea4674b4b720d44cfbae95e37a9f69dccad30
Rawvoid <rawvoid@users.noreply.github.com>
2026-01-15 16:33:36 +0800

    fix: missing @XmlRootElement on Envelope class

--- soap-v1_2/src/main/java/io/github/rawvoid/soap/model/v1_2/Envelope.java
+++ soap-v1_2/src/main/java/io/github/rawvoid/soap/model/v1_2/Envelope.java
@@ -15,6 +15,7 @@ import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAnyAttribute;
 import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.XmlType;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -51,6 +52,7 @@ import lombok.experimental.Accessors;
 @Generated({
     "https://github.com/rawvoid/jaxb-plugins"
 })
+@XmlRootElement(name = "Envelope")
 public class Envelope {
 
     @XmlElement(name = "Header")
